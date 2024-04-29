5a135a8f11f6189a1e85cfd0a4bbeddab658e2ac
HungNguyenDev1511 <nguyenphunghung1511@gmail.com>
2024-04-29 20:49:23 +0700

    change distributed

--- distributed_trainning/multi-worker-training/dataset_car.py
+++ distributed_trainning/multi-worker-training/dataset_car.py
@@ -12,54 +12,53 @@ from sklearn.model_selection import train_test_split
 from IPython.display import Image, clear_output, display
 import matplotlib.pyplot as plt
 from IPython import display
+import ultralytics
 
-def create_datasets():
-    DIR = "/home/hungnguyen/Capstone-Project-Model-Serving/distributed_trainning/data/"
-    IMAGES = DIR +"images/"
-    LABELS = DIR +"labels/"   
 
-    TRAIN = "/home/hungnguyen/Capstone-Project-Model-Serving/distributed_trainning/data/training_images"
-    TEST = "/home/hungnguyen/Capstone-Project-Model-Serving/distributed_trainning/data/testing_images"
+DIR = "/kaggle/working/datasets/cars/"
+IMAGES = DIR +"images/"
+LABELS = DIR +"labels/"
 
-    df = pd.read_csv("/home/hungnguyen/Capstone-Project-Model-Serving/distributed_trainning/data/train_solution_bounding_boxes (1).csv")
-    files = list(df.image.unique())
+TRAIN = "/kaggle/input/car-object-detection/data/training_images"
+TEST = "/kaggle/input/car-object-detection/data/testing_images"
 
-    files_train, files_valid = train_test_split(files, test_size = 0.2)
+df = pd.read_csv("/kaggle/input/car-object-detection/data/train_solution_bounding_boxes (1).csv")
 
-    train_filename = set(files_train)
-    valid_filename = set(files_valid)
-    for file in glob.glob(TRAIN+"/*"):
-        fname =os.path.basename(file)
-        if fname in train_filename:
-            sh.copy(file, IMAGES+"train")
-        elif fname in valid_filename:
-            sh.copy(file, IMAGES+"valid")
+files = list(df.image.unique())
 
-    for _, row in df.iterrows():    
-        image_file = row['image']
-        class_id = "0"
-        x = row['xmin']
-        y = row['ymin']
-        width = row['xmax'] - row['xmin']
-        height = row['ymax'] - row['ymin']
+files_train, files_valid = train_test_split(files, test_size = 0.2)
 
-        x_center = x + (width / 2)
-        y_center = y + (height / 2)
-        x_center /= 676
-        y_center /= 380
-        width /= 676
-        height /= 380
+train_filename = set(files_train)
+valid_filename = set(files_valid)
+for file in glob.glob(TRAIN+"/*"):
+    fname =os.path.basename(file)
+    if fname in train_filename:
+        sh.copy(file, IMAGES+"train")
+    elif fname in valid_filename:
+        sh.copy(file, IMAGES+"valid")
 
-        if image_file in train_filename:   
-            annotation_file = os.path.join(LABELS) + "train/" + image_file.replace('.jpg', '.txt')
-        else:
-            annotation_file = os.path.join(LABELS) + "valid/" + image_file.replace('.jpg', '.txt')
-            
-        with open(annotation_file, 'a') as ann_file:
-            ann_file.write(f"{class_id} {x_center} {y_center} {width} {height}\n")
 
-    
+for _, row in df.iterrows():    
+    image_file = row['image']
+    class_id = "0"
+    x = row['xmin']
+    y = row['ymin']
+    width = row['xmax'] - row['xmin']
+    height = row['ymax'] - row['ymin']
 
-if __name__ == "__main__":
-    create_datasets()
-    
+    x_center = x + (width / 2)
+    y_center = y + (height / 2)
+    x_center /= 676
+    y_center /= 380
+    width /= 676
+    height /= 380
+
+    if image_file in train_filename:   
+        annotation_file = os.path.join(LABELS) + "train/" + image_file.replace('.jpg', '.txt')
+    else:
+        annotation_file = os.path.join(LABELS) + "valid/" + image_file.replace('.jpg', '.txt')
+        
+    with open(annotation_file, 'a') as ann_file:
+        ann_file.write(f"{class_id} {x_center} {y_center} {width} {height}\n")
+
+
