c300fe99b1c8346fa2d88c5426658b4ee2bd29e0
HungNguyenDev1511 <nguyenphunghung1511@gmail.com>
2024-06-26 22:05:54 +0700

    refactor all code again

--- mlflow/pipelines/constants.py
+++
@@ -1,8 +0,0 @@
-# Kubeflow constants
-USERNAME = "user"
-PASSWORD = "Mxthg3uShI"
-NAMESPACE = "kubeflow-user"
-HOST = "http://10.10.10.10:8080"
-
-# MLFLow constants
-TRACKING_URI = "http://192.168.1.7:5001"
